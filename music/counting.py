12d937085e2f2b2a4a926105055efaecb0b8a6de
loukassideris <gokuthelegend@gmail.com>
2023-01-21 19:44:33 +0200

    updates

--- music/counting.py
+++ music/counting.py
@@ -1,20 +1,22 @@
 from pymediainfo import MediaInfo as mi
 import os
 
-path = "/mnt/2A548B6774E73BFC/MyMusic/mp3/export/"
+path = "/mnt/2A548B6774E73BFC/MyMusic/mp3/export_with_warnings/"
 file_list = os.listdir(path)
 
 sampling_rate = []
 channels = []
+duration = []
 
 for file in file_list:
     try:
         parse = mi.parse(path + file)
         sampling_rate.append(parse.audio_tracks[0].sampling_rate)
         channels.append(parse.audio_tracks[0].channel_s)
+        duration.append(parse.audio_tracks[0].duration)
     except IndexError as e:
         print(file, e)
 
 print({_ for _ in sampling_rate})
 print({_ for _ in channels})
-print(len(file_list), len(sampling_rate), len(channels))
+print(len(file_list), len(sampling_rate), len(channels), len(duration))
