cc556ee791cc13a5a4cc4365cd6b7e2aacb19ad2
Λουκάς <loukas.sideris@protonmail.com>
2023-01-26 13:19:52 +0200

    updates

--- generators.py
+++ generators.py
@@ -1,4 +1,4 @@
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5, 6]
 
 a = iter(my_list)
 
@@ -8,10 +8,12 @@ print(next(a))
 print(next(a))
 print(next(a))
 
-#generators
+
+# generators
 def reverse():
     for i in range(5):
         yield i
+
 
 b = reverse()
 
@@ -21,7 +23,7 @@ print(next(b))
 print(next(b))
 print(next(b))
 
-#map functions map(func, *iterables)
+# map functions map(func, *iterables)
 mylist = [10, 20, 30]
 c = map(lambda element: element + 5, mylist)
 
@@ -29,12 +31,15 @@ print(next(c))
 print(next(c))
 print(next(c))
 
-#filter functions filter(func, iterable) yes or no
+# filter functions filter(func, iterable) yes or no
 
 grades = [20, 80, 100, 50, 60, 70]
-def pass_grade(grade):
-    return grade>50
 
-passed = list(filter(lambda grade: grade>50, grades))
+
+def pass_grade(grade):
+    return grade > 50
+
+
+passed = list(filter(lambda grade: grade > 50, grades))
 
 print(passed)
