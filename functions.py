1242703fef0422eae335f771c9cda51f498ccb40
loukassideris <gokuthelegend@gmail.com>
2023-01-26 22:44:16 +0200

    updates

--- functions.py
+++ functions.py
@@ -1,14 +1,19 @@
 def myfunc1(x):
     return x + 5
 
+
 print(myfunc1(10))
+
 
 def myfunc2(x, y, z):
     return x + y + z
 
+
 print(myfunc2(5, 10, 15))
+
 
 def myfunc3(*x):
     return sum(x)
+
 
 print(myfunc3(1, 2, 3, 4))
--- test4.py
+++ test4.py
@@ -1,16 +1,16 @@
 import string
 import random
 
-str_list: str = "" + string.ascii_letters + string.digits + string.punctuation
+str_list: str = string.ascii_letters + string.digits + string.punctuation
 count: int = 0
 
 
-def random_string() -> str:
-    return "".join(random.choices(str_list, k=random.randint(7, 100)))
+def random_string(letters: str) -> str:
+    return "".join(random.choices(letters, k=random.randint(7, 100)))
 
 
 while True:
     count += 1
-    if "pokemon" in random_string() or count >= 10_000_000:
+    if "pokemon" in random_string(str_list) or count >= 10_000_000:
         print(count)
         break
